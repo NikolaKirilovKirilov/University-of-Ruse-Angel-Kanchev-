e641a847436b293f1166f4366e29d1367e4fa1ce
Nikola Kirilov <nikolakirilov145@gmail.com>
2024-05-14 18:39:36 +0300

    Course work 2

--- Third Course Second Semester/Internet Technologies/WebLibrary/src/main/java/beans/Books.java
+++ Third Course Second Semester/Internet Technologies/WebLibrary/src/main/java/beans/Books.java
@@ -4,23 +4,23 @@ import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import java.util.ArrayList;
 
-@Named("Books")
+@Named("books")
 @RequestScoped
 public class Books {
-	private static ArrayList<Book> books;
+	private static ArrayList<Book> bookList;
 
 	public static ArrayList<Book> getBooks() {
-		return books;
+		return bookList;
 	}
 
-	public static void setBooks(ArrayList<Book> books) {
-		books.add(new Book());
+	public void setBooks(ArrayList<Book> books) {
+		this.bookList = books;
 	}
 	
 	public void initBooks()
 	{
-		books.add(new Book("A Million To One","Unknown","Novel","252","D:\\GitHub Repos\\University-of-Ruse-Angel-Kanchev-\\Third Course Second Semester\\Internet Technologies\\WebLibrary\\src\\main\\webapp\\resources\\A-Milion-To-One-Front-Cover.jpg","No Description"));
-		books.add(new Book("City of Orange","Unknown","Novel","132","D:\\GitHub Repos\\University-of-Ruse-Angel-Kanchev-\\Third Course Second Semester\\Internet Technologies\\WebLibrary\\src\\main\\webapp\\resources\\City-Of-Orange-Front-Cover.jpg","No Description"));
-		books.add(new Book("Soul","Unknown","Novel","197","D:\\GitHub Repos\\University-of-Ruse-Angel-Kanchev-\\Third Course Second Semester\\Internet Technologies\\WebLibrary\\src\\main\\webapp\\resources\\Soul-Front-Cover.jpg","No Description"));
+		bookList.add(new Book("A Million To One","Unknown","Novel","252","D:\\GitHub Repos\\University-of-Ruse-Angel-Kanchev-\\Third Course Second Semester\\Internet Technologies\\WebLibrary\\src\\main\\webapp\\resources\\A-Milion-To-One-Front-Cover.jpg","No Description"));
+		bookList.add(new Book("City of Orange","Unknown","Novel","132","D:\\GitHub Repos\\University-of-Ruse-Angel-Kanchev-\\Third Course Second Semester\\Internet Technologies\\WebLibrary\\src\\main\\webapp\\resources\\City-Of-Orange-Front-Cover.jpg","No Description"));
+		bookList.add(new Book("Soul","Unknown","Novel","197","D:\\GitHub Repos\\University-of-Ruse-Angel-Kanchev-\\Third Course Second Semester\\Internet Technologies\\WebLibrary\\src\\main\\webapp\\resources\\Soul-Front-Cover.jpg","No Description"));
 	}
 }
